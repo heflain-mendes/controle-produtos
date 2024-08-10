59d2e0b47fe28edf9916c17add76f4efe5717ab6
Heflain Mendes <heflain@outlook.com.br>
2024-08-10 18:52:50 -0300

    feat: dao genérico

--- dao/clienteDAO.inc.php
+++ dao/clienteDAO.inc.php
@@ -1,30 +1,25 @@
 <?php
 require_once "conexao.inc.php";
+require_once "genericDAO.inc.php";
+require_once "../classes/model/cliente.php";
+
 
 class ClienteDao{
-    private PDO $con;
+    private PDO $conn;
+    private genericDAO $decorator;
 
     public function __construct()
     {
-        $c = new Conexao();
-        $this->con = $c->getConexao();
+        $c = new conexao();
+        $this->conn = $c->getconexao();
+
+        $decorator = new GenericDAO($this->conn, "clientes", Cliente::class);
+
+        $this->decorator = $decorator;
     }
 
-    public function Autenticar($email, $senha) {
-        $sql = $this->con->prepare("select * from clientes where email = :email and senha = :senha");
-
-        $email = strtolower($email);
-        $senha = strtolower($senha);
-
-        $sql->bindValue(":email", $email);
-        $sql->bindValue(":senha", $senha);
-        $sql->execute();
-
-        $cliente = null;
-
-        if($sql->rowCount() == 1){
-            $cliente = $sql->fetch(PDO::FETCH_ASSOC);
-        }
+    public function Autenticar($email, $senha) : Cliente | null {
+        $cliente = $this->decorator->find(["email" => $email, "senha" => $senha]);
 
         return $cliente;
     }
---
+++ dao/genericDAO.inc.php
@@ -0,0 +1,115 @@
+<?php
+
+final class GenericDAO 
+{
+    public function __construct(
+        private PDO $conn, 
+        private string $tableName, 
+        private $className
+    ) {} 
+
+    public function findAll() : array {
+        $sql = $this->conn->prepare(
+        "
+            SELECT * 
+            FROM $this->tableName
+        ");
+
+        $sql->execute();
+
+        $result = null;
+
+        if($sql->rowCount() > 0){
+            $result = $sql->fetchAll(PDO::FETCH_CLASS, $this->className);
+        }
+
+        return $result;
+    }
+
+    public function find(array $fieldsValues) {
+        $findClause = $this->getClause($fieldsValues, " AND ");
+
+        $sql = $this->conn->prepare(
+        "
+            SELECT * 
+            FROM $this->tableName 
+            WHERE " . $findClause
+        );
+
+        foreach ($fieldsValues as $key => $value) {
+            $sql->bindValue(":" . $key, $value);
+        }
+
+        $sql->execute();
+        $result = null;
+
+        if($sql->rowCount() > 0){
+            $sql->setFetchMode(PDO::FETCH_CLASS, $this->className);
+            $result = $sql->fetch(PDO::FETCH_CLASS, $this->className);
+        }
+        
+        return $result;
+    }
+
+    public function insert(array $data) {
+        $collumns = implode(", ", array_keys($data));
+        $values =   ":" . implode(", :", array_keys($data));
+
+        $sql = $this->conn->prepare(
+        "
+            INSERT INTO $this->tableName ($collumns) 
+            VALUES ($values)
+        "
+        );
+
+        foreach ($data as $key => $value) {
+            $sql->bindValue(":" . $key, $value);
+        }
+
+        $sql->execute();
+    }
+
+    public function update(array $data, array $fieldsValues) {
+        $setClause = $this->getClause($data, ", ");
+        $whereClause = $this->getClause($fieldsValues, " AND ");
+
+        $sql = $this->conn->prepare(
+            "UPDATE $this->tableName 
+            SET " . $setClause . 
+            "WHERE " . $whereClause
+        );
+
+        foreach ($fieldsValues as $key => $value) {
+            $sql->bindValue(":" . $key, $value);
+        }
+
+        foreach ($data as $key => $value) {
+            $sql->bindValue($key, $value);
+        }
+
+        $sql->execute();
+    }
+
+    public function delete(array $fieldsValues) {
+        $findClause = $this->getClause($fieldsValues, " AND ");
+
+        $sql = $this->conn->prepare("DELETE FROM $this->tableName WHERE " . $findClause);
+
+        foreach ($fieldsValues as $key => $value) {
+            $sql->bindValue(":" . $key, $value);
+        }
+
+        $sql->execute();
+    }
+
+    private function getClause(array $fieldsValues, string $sep) : string{
+        $clauses = [];
+
+        foreach (array_keys($fieldsValues) as $key) {
+            $clauses[] = "$key = :$key";
+        }
+
+        return implode($sep, $clauses);
+    }
+}
+?>
